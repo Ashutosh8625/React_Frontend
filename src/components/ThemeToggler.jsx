8470210b9bb2111b356df0721e8d6cc20239386c
Ashutosh Tiwari <ashutoshtiwari4437@gmail.com>
2026-02-05 20:19:52 +0530

    added useTheme

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react'
+import {createContext, useContext,useState} from 'react'
 
 const ThemeContext = createContext();
 
@@ -14,6 +14,16 @@ export function ThemeProvider({ children }){
     isDark: theme === 'dark'
   }
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+}
+
+export function useTheme(){
+  const context = useContext(ThemeContext)
+
+  if(!context) {
+    throw new Error("useTheme must be used within ThemeProvider")
+  }
+
+  return context
 }
 
 function ThemeToggler() {
